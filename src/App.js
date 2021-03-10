2cde68c92ebe25bbfeace4d63a131b3f208f4225
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-10 11:12:26 -0300

    Aula 53 - Styled Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Container, Header, BemVindo} from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+          <a className="titulo">Projeto Styled</a>
+      </Header>
+      <BemVindo cor="FF0000" tamanho={35}>
+        Bem-vindo ao sistema!
+      </BemVindo>
+    </Container>
   );
 }
 
 export default App;
+
+/* <div className="container">
+<header className="header">
+  
+</header>
+
+</div> */
